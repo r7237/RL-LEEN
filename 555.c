92b4f09ff870f648a5f841712f16fb9e8666cecb
r7237 <35914754+r7237@users.noreply.github.com>
2018-03-05 13:24:23 +0530

    Create 555.c

---
+++ 555.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int n;
+ 
+   printf("Enter an integern");
+   scanf("%d", &n);
+ 
+   if (n%2 == 0)
+      printf("Evenn");
+   else
+      printf("Oddn");
+ 
+   return 0;
+}
